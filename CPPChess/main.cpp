beb9d3fb8468aae80251d7ad47f18112697b69db
krishkalai <krishkalai07@gmail.com>
2017-10-21 22:17:04 -0700

    Initial Commit

---
+++ CPPChess/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CPPChess
+//
+//  Created by Krish Kalai on 10/21/17.
+//  Copyright © 2017 krishkalai07. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
